79ed9d81d48d9c3c080ced82177783c9c1db761a
Mzwakhilet@gmail.com <Mzwakhilet@gmail.com>
2019-06-13 11:21:14 +0200

    Added GetAllJobs that brings the list oft jobs created by a particular client only

--- LogWebApi/LogWebApi/Controllers/ClientJobController.cs
+++ LogWebApi/LogWebApi/Controllers/ClientJobController.cs
@@ -9,6 +9,7 @@ namespace LogWebApi.Controllers
 {
     public class ClientJobController : ApiController
     {
+
         [HttpPost]
         public HttpResponseMessage CreateLog(JobModel jobModel)
         {
@@ -33,6 +34,43 @@ namespace LogWebApi.Controllers
                 message.Headers.Location = new Uri(Request.RequestUri + "/" + jobModel.ID);
 
                 return message;
+            }
+        }
+
+        public List<JobModel> GetAllJobs(int Id)
+        {
+            List<JobModel> jobList = new List<JobModel>();
+            using (DbEntities entities = new DbEntities())
+            {
+                var result = (from j in entities.Jobs
+                              where j.client_user_no == Id
+
+                              select j).ToList<Job>();
+
+
+
+                if (result != null)
+                {
+                    foreach (var item in result)
+                    {
+                        jobList.Add(new JobModel
+                        {
+                            ID = item.job_No,
+                            Title = item.job_title,
+                            Description = item.job_description,
+                            Date = item.job_date,
+                            Attachment = item.job_attachement,
+                            ClientID = item.client_user_no,
+                            Status = item.job_status
+                        });
+                    }
+                    return jobList;
+
+                }
+                else
+                {
+                    return null;
+                }
             }
         }
     }
